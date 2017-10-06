615b23ced3f549cc9e80d2d252917bd9935843df
Ross Dunford <dunford@Rus-MacBook.local>
2017-10-06 13:30:25 -0600

    added button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+import './Login.css';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div id='root'>
+                <div className='Auth_Parent'>
+                    <div className=''>
+                        <a href={process.env.REACT_APP_LOGIN}><button>Login</button></a>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
