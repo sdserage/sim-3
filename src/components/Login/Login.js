dcbc11c774e44d41cae9364422bcaff9708cc0b9
Ross Dunford <dunford@Rus-MacBook.local>
2017-10-06 15:59:53 -0600

    updating

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
-
+import loginlogo from "../../assets/logo.png"
 
 export default class Login extends Component {
     render() {
@@ -8,14 +8,15 @@ export default class Login extends Component {
             <div id='root'>
                 <div className='Auth_parent_container'>
                     <div className='Auth_child_container'>
-                        <div className = 'Auth_logo_container'>
-                            <img class="Auth_logo_img" src="../" alt="logo"/>
+                        <div className='Auth_logo_container'>
+                            <img class="Auth_logo_img" src={loginlogo} />
                             <div className="Auth_logo_text open-sans-bold">Helo</div>
                         </div>
-                        <div className = 'Log_inBtn'>
-                            <a href={process.env.REACT_APP_LOGIN}><button>Login/Register</button></a>
+                        <div className='Auth_link_container'>
+                            <div className='Auth_auth0_div open-sans black-bgc'>
+                                <a className="Auth_auth0_link open-sans open-sans" href={process.env.REACT_APP_LOGIN}>Login/Register</a>
+                            </div>
                         </div>
-
                     </div>
                 </div>
 
