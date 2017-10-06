fe82ef7e23a5691954b790209d70635b510a4f71
Ross Dunford <dunford@Rus-MacBook.local>
2017-10-06 15:18:01 -0600

    updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,10 +9,10 @@ export default class Login extends Component {
                 <div className='Auth_parent_container'>
                     <div className='Auth_child_container'>
                         <div className = 'Auth_logo_container'>
-                            <img class="Auth_logo_img" src="" alt="logo"/>
+                            <img class="Auth_logo_img" src="../" alt="logo"/>
                             <div className="Auth_logo_text open-sans-bold">Helo</div>
                         </div>
-                        <div className = 'button'>
+                        <div className = 'Log_inBtn'>
                             <a href={process.env.REACT_APP_LOGIN}><button>Login/Register</button></a>
                         </div>
 
